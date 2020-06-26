25c5d23bedd7897285bb1d15bfd16ad115621bf1
Jethro85 <52518717+Jethro85@users.noreply.github.com>
2020-06-25 22:31:20 -0400

    Update exchange_testing_dataset.py

--- Coverage and Robustness VS. Training Datasets/exchange_testing_dataset.py
+++ Coverage and Robustness VS. Training Datasets/exchange_testing_dataset.py
@@ -4,10 +4,10 @@ import numpy as np
 def load_data(name):
     assert (name.upper() in ['MNIST', 'CIFAR', 'SVHN'])
     name = name.lower()
-    x_train = np.load('./data/' + name + '_data/' + name + '_x_train.npy')
-    y_train = np.load('./data/' + name + '_data/' + name + '_y_train.npy')
-    x_test = np.load('./data/' + name + '_data/' + name + '_x_test.npy')
-    y_test = np.load('./data/' + name + '_data/' + name + '_y_test.npy')
+    x_train = np.load('../data/' + name + '_data/' + name + '_x_train.npy')
+    y_train = np.load('../data/' + name + '_data/' + name + '_y_train.npy')
+    x_test = np.load('../data/' + name + '_data/' + name + '_x_test.npy')
+    y_test = np.load('../data/' + name + '_data/' + name + '_y_test.npy')
     return x_train, y_train, x_test, y_test
 
 if __name__ == '__main__':
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     # ## load mine trained model
     from keras.models import load_model
 
-    model = load_model('./data/' + dataset + '_data/model/' + model_name + '.h5')
+    model = load_model('../data/' + dataset + '_data/model/' + model_name + '.h5')
     model.summary()
 
     index = np.load('fuzzing/nc_index_test_{}.npy'.format(0), allow_pickle=True).item()
